d93cfc98f85c6f9fd5fe7ad3d318f089516fede4
Zhehao Mao <zhehao.mao@gmail.com>
2011-07-29 17:52:08 -0400

    added PUT DELETE HEAD

--- websh.c
+++ websh.c
@@ -32,12 +32,31 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+int handle_upload(void * ptr, size_t size, size_t nmemb, void * userdata){
+	int len = size * nmemb;
+	upload_buffer * upbuf = (upload_buffer*)userdata;
+	
+	if(upbuf->index + len < upbuf->length){
+		memcpy(ptr, upbuf->data, len);
+		upbuf->index += len;
+	} else if(upbuf->index < upbuf->length){
+		len = upbuf->length - upbuf->index;
+		upbuf->index = upbuf->length;
+		memcpy(ptr, upbuf->data, len);
+	} else {
+		len = 0;
+	}
+	
+	return len;
+}
+
 void handle_input(char * url, char * input){
 	vector * vec = str_split(input, " ");
 	CURL * curl;
 	char *com, *path;
 	char *data = NULL;
 	char * full_url;
+	upload_buffer upbuf;
 	
 	if(vec->length < 2){
 		destroy_vector(vec);
@@ -62,11 +81,31 @@ void handle_input(char * url, char * input){
 		curl_easy_setopt(curl, CURLOPT_POST, 1);
 		data = linenoise("...");
 		curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, data);
-		free(data);
-	} 
+	} else if (strcmp(com, "HEAD")==0){
+		curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+	} else if (strcmp(com, "PUT")==0){
+		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1);
+		data = linenoise("...");
+		upbuf.data = data;
+		upbuf.index = 0;
+		upbuf.length = strlen(data);
+		curl_easy_setopt(curl, CURLOPT_READDATA, &upbuf);
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, handle_upload);
+	} else if(strcmp(com, "DELETE")==0) {
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+	} else {
+		free(full_url);
+		curl_easy_cleanup(curl);
+		destroy_vector(vec);
+	}
 	
 	curl_easy_perform(curl);
+	/* print a newline incase the response has no ending newline */
+	printf("\n");
 	
+	if(data != NULL)
+		free(data);
+		
 	free(full_url);
 	curl_easy_cleanup(curl);
 	destroy_vector(vec);
--- websh.h
+++ websh.h
@@ -1,1 +1,8 @@
+typedef struct{
+	char * data;
+	int index;
+	int length;
+} upload_buffer;
+
 void handle_input(char * url, char * input);
+int handle_upload(void * ptr, size_t size, size_t nmemb, void * userdata);
